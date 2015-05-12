cda8e924a3a492cf88ce7e52cb45994f1787a2a9
Robin Deits <robin.deits@gmail.com>
2015-05-12 15:44:23 -0400

    allow torque deflection gains to be used for all joints

--- estimate_tools/src/backlash_filter_tools/torque_adjustment.cpp
+++ estimate_tools/src/backlash_filter_tools/torque_adjustment.cpp
@@ -2,13 +2,17 @@
 // by IHMC
 
 #include <estimate_tools/torque_adjustment.hpp>
-//#include "atlas/AtlasJointNames.h"
-//#include "atlas/AtlasControlTypes.h"
-//#include "atlas/AtlasJointNames.h"
+#include <cmath>
 
 namespace EstimateTools {
 
 TorqueAdjustment::TorqueAdjustment(std::vector<float> k_):k_(k_){
+  // Construct a torque adjustment tool with the given spring constants. The
+  // vector k_ should be the same length as the number of efforts and
+  // positions (and in the same order), and is measured in units of radians
+  // per Newton-meter. To prevent torque adjustment on a joint, set its
+  // associated value of k_ to inf (since a joint with no deflection can be
+  // thought of as an infinitely stiff spring).
   std::cout << "TorqueAdjustment gains: ";
 
   for( size_t i=0; i<k_.size(); i++)
@@ -29,28 +33,12 @@ float TorqueAdjustment::magnitudeLimit(float val_in){
 }
 
 void TorqueAdjustment::processSample(std::vector<float> &position, std::vector<float> &effort ){
-  //return;
-
-  // // tmp testing
-  // int JOINT_L_LEG_HPZ   = 4;
-  // int JOINT_L_LEG_HPX   = 5;
-  // int JOINT_L_LEG_HPY   = 6;
-  // int JOINT_L_LEG_KNY   = 7;
-  // int JOINT_L_LEG_AKY   = 8;
-  // int JOINT_L_LEG_AKX   = 9;
-  // int JOINT_R_LEG_HPZ   = 10;
-  // int JOINT_R_LEG_HPX   = 11;
-  // int JOINT_R_LEG_HPY   = 12;
-  // int JOINT_R_LEG_KNY   = 13;
-  // int JOINT_R_LEG_AKY   = 14;
-  // int JOINT_R_LEG_AKX   = 15;
-
-
   for (size_t i=0; i< k_.size(); i++){
-    position[4+i] -= magnitudeLimit( effort[4+i] / k_[i]);
-    position[10+i] -= magnitudeLimit( effort[10+i] / k_[i]);
+    if (std::isnormal(k_[i])) {
+      // Don't do the correction if k_[i] is zero, NaN, or infinite. 
+      position[i] -= magnitudeLimit( effort[i] / k_[i]);
+    }
   }
-
 
   return;
 
@@ -62,24 +50,8 @@ void TorqueAdjustment::processSample(std::vector<float> &position, std::vector<f
   // Huge effect - for testing:
   //double k_hpz = 700;
   //double k     = 1000;
-
-
-  position[10] -= magnitudeLimit( effort[10] / k_hpz );
-  position[11] -= magnitudeLimit( effort[11] / k_x );
-  position[12] -= magnitudeLimit( effort[12] / k_x );
-
-  position[13] -= magnitudeLimit( effort[13] / k_x );
-  position[14] -= magnitudeLimit( effort[14] / k_x );
-  position[15] -= magnitudeLimit( effort[15] / k_x );
-
-  position[4] -= magnitudeLimit( effort[4] / k_hpz );
-  position[5] -= magnitudeLimit( effort[5] / k_x );
-  position[6] -= magnitudeLimit( effort[6] / k_x );
-
-  position[7] -= magnitudeLimit( effort[7] / k_x );
-  position[8] -= magnitudeLimit( effort[8] / k_x );
-  position[9] -= magnitudeLimit( effort[9] / k_x );
   */
+
 }
 
 }
