34ee0b7082c4d3de4a459f3f8772620f28eb6a1a
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-08-11 17:25:35 +0100

    adding velodyne support for 2-D scan matching

--- lidar_odometry/src/lidar_odometry/lidar-odometry.cpp
+++ lidar_odometry/src/lidar_odometry/lidar-odometry.cpp
@@ -172,6 +172,113 @@ void LidarOdom::doOdometry(float* ranges, int nranges, float rad0, float radstep
     //cleanup
     free(points);
 }
+
+
+/////// CODE BELOW IS TO ACCEPT A SET OF X,Y POINTS AND TREAT THEM AS LIDAR RETURNS
+/////// ORIGINALLY USED FOR HORIZONTAL DATA COMING FROM A VELODYNE
+int projectPointsToPoints(std::vector<float> x, std::vector<float> y, int numPoints,
+    frsmPoint * points, double maxRange = 1e10, 
+    double * aveValidRange = NULL, double * stddevValidRange = NULL)
+{
+  int count = 0;
+  double aveRange = 0;
+  double aveRangeSq = 0;
+
+  for (int i = 0; i < numPoints; i++) {
+    double range = sqrt( x[i]*x[i] + y[i]*y[i] );
+    if (range > .1 && range < maxRange ) {
+      points[count].x = x[i];
+      points[count].y = y[i];
+      count++;
+      aveRange += range;
+      aveRangeSq += frsm_sq(range);
+    }
+  }
+
+  aveRange /= (double) count;
+  aveRangeSq /= (double) count;
+
+  if (aveValidRange != NULL)
+    *aveValidRange = aveRange;
+  if (stddevValidRange != NULL)
+    *stddevValidRange = sqrt(aveRangeSq - frsm_sq(aveRange));
+
+  return count;
+}
+
+
+int projectPointsAndDecimate(int beamskip, float spatialDecimationThresh, std::vector<float> x, std::vector<float> y, int npoints,
+    frsmPoint * points, double maxRange = 1e10)
+{
+  int lastAdd = -1000;
+  double aveRange;
+  double stdDevRange;
+
+  int numValidPoints = projectPointsToPoints(x, y, npoints,
+    points, maxRange, &aveRange, &stdDevRange);
+
+  frsmPoint origin = { 0, 0 };
+  frsmPoint lastAddPoint = { 0, 0 };
+  int numDecPoints = 0;
+  for (int i = 0; i < numValidPoints; i++) {
+    //add every beamSkip beam, unless points are more than spatialDecimationThresh, or more than 1.8 stdDevs more than ave range
+    if ((i - lastAdd) > beamskip || frsm_dist(&points[i], &lastAddPoint) > spatialDecimationThresh || frsm_dist(&points[i],
+        &origin) > (aveRange + 1.8 * stdDevRange)) {
+      lastAdd = i;
+      lastAddPoint = points[i];
+      points[numDecPoints] = points[i]; // ok since i >= numDecPoints
+      numDecPoints++;
+    }
+  }
+  return numDecPoints;
+
+}
+
+
+void LidarOdom::doOdometry(std::vector<float> x, std::vector<float> y, int npoints, int64_t utime){
+
+    double maxRange = 39.0;
+    int beamSkip = 3;
+
+    //Project ranges into points, and decimate points so we don't have too many
+    frsmPoint * points = (frsmPoint *) calloc(npoints, sizeof(frsmPoint));
+    int numValidPoints = projectPointsAndDecimate(beamSkip,
+            spatialDecimationThresh_, x, y, npoints, points, maxRange);
+    if (numValidPoints < 30) {
+        fprintf(stderr, "WARNING! NOT ENOUGH VALID POINTS! numValid=%d\n",
+                numValidPoints);
+        free(points);
+        return;
+    }
+
+    //Actually do the matching
+    ScanTransform r = sm_->matchSuccessive(points, numValidPoints,
+            laserType_, utime, NULL); //don't have a better estimate than prev, so just set prior to NULL
+                                      //utime is ONLY used to tag the scans that get added to the map, doesn't actually matter
+    Eigen::Isometry3d r_Iso = getScanTransformAsIsometry3d(r);
+    prevOdom_ = currOdom_;
+    currOdom_ = r_Iso;
+    prevUtime_ = currUtime_;
+    currUtime_ = utime;
+
+    //Print current position periodically
+    static double lastPrintTime = 0;
+    if (frsm_get_time() - lastPrintTime > 2.0) {
+        lastPrintTime = frsm_get_time();
+        fprintf(stderr,
+                "x=%+7.3f y=%+7.3f t=%+7.3f\t score=%f hits=%.2f sx=%.2f sxy=%.2f sy=%.2f st=%.2f, numValid=%d\n",
+                r.x, r.y, r.theta, r.score, (double) r.hits / (double) numValidPoints, r.sigma[0], r.sigma[1],
+                r.sigma[4], r.sigma[8], numValidPoints);
+    }
+
+    //Do drawing periodically
+    // This was disabled when moving to the frsm library. it would be easily re-added on a fork
+    //Do drawing periodically!
+    if (doDrawing_ && frsm_get_time() - lastDrawTime_ > .2) {
+        lastDrawTime_ = frsm_get_time();
+        //frsm_tictoc("drawing");
+        draw(points, numValidPoints, &r);
+        //frsm_tictoc("drawing");
     }
 
     //cleanup
