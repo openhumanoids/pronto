ba84a5bf01abc16e57f350b907b58ee1cdb0103d
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-08-13 10:20:02 +0100

    adding renderering lidar in old gtk viewer

--- pronto-utils/src/tools/pointcloud2_tool.cpp
+++ pronto-utils/src/tools/pointcloud2_tool.cpp
@@ -1,20 +1,19 @@
 #include <lcm/lcm-cpp.hpp>
 
-//#include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/passthrough.h>
+
+#include <path_util/path_util.h>
+#include <laser_utils/laser_util.h>
 
 #include <lcmtypes/pronto/pointcloud2_t.hpp>
 #include <lcmtypes/pronto/pointcloud_t.hpp>
 
 #include <pronto_utils/pronto_vis.hpp> // visualize pt clds
-#include <ConciseArgs>
-
-#include <path_util/path_util.h>
-
 #include <pronto_utils/conversions_lcm.hpp>
 
+#include <ConciseArgs>
 
 
 namespace pcl
@@ -43,11 +42,11 @@ struct CommandLineConfig
   bool verbose;
 };
 
-class StereoOdom{
+class App{
   public:
-    StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_recv_, boost::shared_ptr<lcm::LCM> &lcm_pub_, const CommandLineConfig& cl_cfg_);
+    App(boost::shared_ptr<lcm::LCM> &lcm_recv_, boost::shared_ptr<lcm::LCM> &lcm_pub_, const CommandLineConfig& cl_cfg_);
     
-    ~StereoOdom(){
+    ~App(){
     }
 
   private:
@@ -56,30 +55,66 @@ class StereoOdom{
     boost::shared_ptr<lcm::LCM> lcm_recv_;
     boost::shared_ptr<lcm::LCM> lcm_pub_;
 
+    pronto_vis* pc_vis_ ;
+    BotParam* botparam_;
+    BotFrames* botframes_;
+
     void viconHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  pronto::pointcloud2_t* msg);
 };    
 
-StereoOdom::StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_recv_, boost::shared_ptr<lcm::LCM> &lcm_pub_, const CommandLineConfig& cl_cfg_) : 
+App::App(boost::shared_ptr<lcm::LCM> &lcm_recv_, boost::shared_ptr<lcm::LCM> &lcm_pub_, const CommandLineConfig& cl_cfg_) : 
        cl_cfg_(cl_cfg_), lcm_recv_(lcm_recv_), lcm_pub_(lcm_pub_)
 {
 
-  lcm_recv_->subscribe("VELODYNE",&StereoOdom::viconHandler,this);
-  cout <<"StereoOdom Constructed\n";
+  lcm_recv_->subscribe("VELODYNE",&App::viconHandler,this);
+  cout <<"App Constructed\n";
+
+  bool reset = 1;
+  pc_vis_ = new pronto_vis( lcm_pub_->getUnderlyingLCM() );
+  // obj: id name type reset
+  // pts: id name type reset objcoll usergb rgb
+  pc_vis_->obj_cfg_list.push_back( obj_cfg(70000,"Pose - Velodyne",5,reset) );
+  pc_vis_->ptcld_cfg_list.push_back( ptcld_cfg(70001,"Cloud - Velodyne"         ,1,reset, 70000,1, {0.0, 0.0, 1.0} ));
+
+  botparam_ = bot_param_new_from_server(lcm_recv_->getUnderlyingLCM(), 0);
+  botframes_= bot_frames_get_global(lcm_recv_->getUnderlyingLCM(), botparam_);
+
 }
 
+int get_trans_with_utime(BotFrames *bot_frames,
+        const char *from_frame, const char *to_frame, int64_t utime,
+        Eigen::Isometry3d & mat){
+  int status;
+  double matx[16];
+  status = bot_frames_get_trans_mat_4x4_with_utime( bot_frames, from_frame,  to_frame, utime, matx);
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      mat(i,j) = matx[i*4+j];
+    }
+  }  
+  return status;
+}
 
+void App::viconHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  pronto::pointcloud2_t* msg){
 
-void StereoOdom::viconHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  pronto::pointcloud2_t* msg){
-   cout <<"got vdyne\n";
-
+  // 1. convert to a pcl point cloud:
   pcl::PointCloud<pcl::PointXYZIR>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZIR> ());
   pcl::fromLCMPointCloud2( *msg, *cloud);
   //pcl::io::savePCDFileASCII ("mm.pcd", *cloud);
 
+
+  // 2. republish the velodyne as a collections message (on the velodyne frame
+  Eigen::Isometry3d velodyne_to_local;
+  get_trans_with_utime( botframes_ , "VELODYNE", "local"  , msg->utime, velodyne_to_local);
+  Isometry3dTime velodyne_to_local_T = Isometry3dTime(msg->utime, velodyne_to_local);
+  pc_vis_->pose_to_lcm_from_list(70000, velodyne_to_local_T);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_xyzrgb (new pcl::PointCloud<pcl::PointXYZRGB> ());
+  pcl::copyPointCloud(*cloud, *cloud_xyzrgb);
+  pc_vis_->ptcld_to_lcm_from_list(70001, *cloud_xyzrgb, msg->utime, msg->utime);  
+
+  // 3. Find the horizontal lidar beam - could use the ring value but PCL filter doesnt handle it
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz (new pcl::PointCloud<pcl::PointXYZ> ());
   pcl::copyPointCloud(*cloud, *cloud_xyz);
-
-  // Find the horizontal lidar beam - could use the ring value but PCL filter doesnt handle it
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PassThrough<pcl::PointXYZ> pass;
   pass.setInputCloud (cloud_xyz);
@@ -89,7 +124,8 @@ void StereoOdom::viconHandler(const lcm::ReceiveBuffer* rbuf, const std::string&
   pass.filter (*cloud_filtered);
   //pcl::io::savePCDFileASCII ("mm_filtered.pcd", *cloud_filtered);
 
-  // republish beam as new message
+
+  // 4. republish beam as new message
   pronto::pointcloud_t out;
   out.utime = msg->utime;
   out.seq = msg->seq;
@@ -101,7 +137,7 @@ void StereoOdom::viconHandler(const lcm::ReceiveBuffer* rbuf, const std::string&
   }
   out.n_points = out.points.size();
   out.n_channels = 0;
-  lcm_pub_->publish("VELODYNE_HORIZONTAL",&out);
+//  lcm_pub_->publish("VELODYNE_HORIZONTAL",&out);
 
 }
 
@@ -120,6 +156,6 @@ int main(int argc, char **argv){
   lcm_recv = boost::shared_ptr<lcm::LCM>(new lcm::LCM);
   lcm_pub = boost::shared_ptr<lcm::LCM>(new lcm::LCM);
 
-  StereoOdom fo= StereoOdom(lcm_recv, lcm_pub, cl_cfg);
+  App fo= App(lcm_recv, lcm_pub, cl_cfg);
   while(0 == lcm_recv->handle());
 }
