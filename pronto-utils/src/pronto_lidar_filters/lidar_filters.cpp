cbaef32f00898b21ba0be0752dcd5087fcd9b9e7
Georgi Tinchev <georgi.tinchev@ed.ac.uk>
2017-03-21 15:05:37 +0000

    Removed unnecessary functions.

--- pronto-utils/src/pronto_lidar_filters/lidar_filters.cpp
+++ pronto-utils/src/pronto_lidar_filters/lidar_filters.cpp
@@ -13,42 +13,6 @@ pronto_lidar_filters::pronto_lidar_filters(float minLimit_, float maxLimit_, flo
  * @param msg     incomming planar_lidar_t message
  * @param msg_out filtered planar_lidar_t message
  */
-void pronto_lidar_filters::removeOuterPoints(const bot_core::planar_lidar_t *msg, bot_core::planar_lidar_t & msg_out) {
-
-  msg_out.utime = msg->utime;
-
-  msg_out.radstep = msg->radstep;
-
-  // difference between the wanted lower angle and the one in the message
-  float dlr = fabs(msg->rad0 - minLimit_); // difference in radians
-
-  int lb = round(dlr / msg->radstep);
-
-  // (final angle of the sensor) - maxLimit
-  float dur = (msg->rad0 + msg->nranges*msg->radstep) - maxLimit_;
-
-  // array upper bound index
-  int ub = msg->nranges - round(dur/msg->radstep);
-
-  msg_out.nranges = ub-lb; // total ranges
-  msg_out.nintensities = ub-lb; // total intensities
-
-  msg_out.rad0 = msg->rad0 + lb*msg->radstep; // calculate the new starting angle
-
-  for (int i=lb;i<ub; ++i) {
-    msg_out.ranges.push_back(msg->ranges[i]);
-    msg_out.intensities.push_back(msg->intensities[i]);
-  }
-
-}
-
-/**
- * Cut the first and last X points from a received message
- * Configuration for X is defined by minLimit_ and maxLimit_
- *
- * @param msg     incomming planar_lidar_t message
- * @param msg_out filtered planar_lidar_t message
- */
 void pronto_lidar_filters::removeOuterPoints(const bot_core::planar_lidar_t msg, bot_core::planar_lidar_t & msg_out) {
 
   msg_out.utime = msg.utime;
@@ -75,45 +39,6 @@ void pronto_lidar_filters::removeOuterPoints(const bot_core::planar_lidar_t msg,
     msg_out.ranges.push_back(msg.ranges[i]);
     msg_out.intensities.push_back(msg.intensities[i]);
   }
-
-}
-
-/**
- * Cut the first and last X points from a received message
- * Configuration for X is defined by minLimit_ and maxLimit_
- *
- * @param  msg incomming message
- * @return     filtered message
- */
-bot_core::planar_lidar_t pronto_lidar_filters::removeOuterPoints(const bot_core::planar_lidar_t *msg) {
-
-  bot_core::planar_lidar_t msg_out;
-  msg_out.utime = msg->utime;
-
-  msg_out.radstep = msg->radstep;
-
-  // difference between the wanted lower angle and the one in the message
-  float dlr = fabs(msg->rad0 - minLimit_); // difference in radians
-
-  int lb = round(dlr / msg->radstep);
-
-  // (final angle of the sensor) - maxLimit
-  float dur = (msg->rad0 + msg->nranges*msg->radstep) - maxLimit_;
-
-  // array upper bound index
-  int ub = msg->nranges - round(dur/msg->radstep);
-
-  msg_out.nranges = ub-lb; // total ranges
-  msg_out.nintensities = ub-lb; // total intensities
-
-  msg_out.rad0 = msg->rad0 + lb*msg->radstep; // calculate the new starting angle
-
-  for (int i=lb;i<ub; ++i) {
-    msg_out.ranges.push_back(msg->ranges[i]);
-    msg_out.intensities.push_back(msg->intensities[i]);
-  }
-
-  return msg_out;
 
 }
 
--- pronto-utils/src/pronto_lidar_filters/lidar_filters.hpp
+++ pronto-utils/src/pronto_lidar_filters/lidar_filters.hpp
@@ -10,9 +10,7 @@ public:
   pronto_lidar_filters(float minLimit, float maxLimit, float minRange, float maxRange);
   ~pronto_lidar_filters();
 
-  void removeOuterPoints(const bot_core::planar_lidar_t *msg, bot_core::planar_lidar_t &msg_out);
   void removeOuterPoints(const bot_core::planar_lidar_t msg, bot_core::planar_lidar_t &msg_out);
-  bot_core::planar_lidar_t removeOuterPoints(const bot_core::planar_lidar_t *msg);
   bot_core::planar_lidar_t removeRangePoints(const bot_core::planar_lidar_t *msg);
 
 private:
